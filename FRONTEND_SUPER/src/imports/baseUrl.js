5ea77a8cfb9ebde8c453649b6e61ac2a8c4b8380
pratiksontakke <pratikass488@gmail.com>
2025-08-16 19:31:00 +0530

    update folder struc

--- FRONTEND_SUPER/src/imports/baseUrl.js
+++
@@ -1,4 +0,0 @@
-// const BACKEND_URL = "https://taskpal.duckdns.org/";
-const BACKEND_URL = "http://localhost:5000/";
-
-export default BACKEND_URL;
--- FRONTEND_SUPER/src/imports/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const reduxLocalStorage = "persistStore";
-
-export const getToken = () => {
-  const serialState = localStorage.getItem(reduxLocalStorage);
-  const token = JSON.parse(serialState)?.user?.token??null
-  return token;
-};
